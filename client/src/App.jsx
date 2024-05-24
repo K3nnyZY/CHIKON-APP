e409b61fa1b579a29c6927d13775d5bab48f245e
K3nnyZY <kennyzhuye15@gmail.com>
2024-05-24 07:33:31 -0500

    c

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigation } from "./routes";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello word</h1>
+      <Navigation />
+    </div>
+  );
+}
