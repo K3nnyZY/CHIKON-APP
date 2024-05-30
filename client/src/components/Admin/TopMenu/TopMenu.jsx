80d38afb4365b22a8174c236f64c0ddc8470fb9a
K3nnyZY <kennyzhuye15@gmail.com>
2024-05-30 15:47:44 -0500

    changes

--- client/src/components/Admin/TopMenu/TopMenu.jsx
+++ client/src/components/Admin/TopMenu/TopMenu.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {Icon, Menu} from "semantic-ui-react"
 import {useAuth} from "../../../hooks"
+import logo from "../../../assets/logo1.png"
 import "./TopMenu.scss"
+import { Link } from 'react-router-dom'
 
 export function TopMenu() {
     const {auth, logout} = useAuth();
@@ -16,7 +18,10 @@ export function TopMenu() {
   return (
     <Menu fixed='top' className='top-menu-admin'>
         <Menu.Item className='top-menu-admin__logo'>
-            <p>Chikon Admin</p>
+            <img src={logo} alt='Chikon Admin'/>
+            <Link to = '/admin'>
+            <p>&nbsp;&nbsp;&nbsp; Chikon Admin</p>
+            </Link>
         </Menu.Item>
 
         <Menu.Menu position='right'>
